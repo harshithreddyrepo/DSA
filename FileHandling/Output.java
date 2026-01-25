797ad6aeea826625b4027f6666e28faf02021933
harshith <harshith.akkati@gmail.com>
2026-01-25 17:58:42 +0530

    File Handling in Java

--- FileHandling/Output.java
+++ FileHandling/Output.java
@@ -1,2 +1,36 @@
-package DSA_with_kunal.FileHandling;public class Output {
+package DSA_with_kunal.FileHandling;
+
+import java.io.*;
+
+class Output {
+    static void output() {
+        // output
+        OutputStream os = System.out;
+        // os.write(😍); // range is exceeded
+
+        try (OutputStreamWriter osw = new OutputStreamWriter(System.out)) {
+            osw.write("Hello World");
+            osw.write(97);
+            osw.write(10);
+            osw.write('A');
+            osw.write('\n');
+            char[] arr = "hello world".toCharArray();
+            osw.write(arr);
+            // osw.write(😍);
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+
+        try (FileWriter fw = new FileWriter("note.txt", true)) {
+            fw.write("this should be appended");
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+
+        try (BufferedWriter bw = new BufferedWriter(new FileWriter("note.txt"))) {
+            bw.write("Hare Krishna");
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+    }
 }
